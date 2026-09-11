82d7d633f7992528ff21ae89fd16af4bef902482
haddy256 <heinhtetkhaing52@gmail.com>
2026-09-11 21:27:28 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from upstash_redis import Redis
+
+load_dotenv()
+
+redis = Redis(
+    url=os.getenv("UPSTASH_REDIS_REST_URL"),
+    token=os.getenv("UPSTASH_REDIS_REST_TOKEN")
+)
+
+redis.set("foo", "bar")
+value = redis.get("foo")
+print(value)
